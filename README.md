b7480f90226b1f8a668909cd5fc5aebfc3b421a8
Aiden McClelland <aiden.mcclelland@saltlending.com>
2019-09-30 20:19:51 -0600

    Document

---
+++ README.md
@@ -0,0 +1,10 @@
+# zeroize_alloc
+This crate zeros all memory before freeing it, so if you keep secrets on the heap, you can be sure they no longer exist once they are freed.
+
+## Usage
+rust```
+#[global_allocator]
+static ALLOC: zeroize_alloc::ZeroizingAlloc<YourAllocator> = ZeroizingAlloc(YourAllocator);
+```
+
+If you want to use the default allocator, use `std::alloc::System`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,56 @@
-use std::alloc::{GlobalAlloc, Layout, System};
+#![no_std]
 
-struct ZeroizingAllocator;
+use core::alloc::{GlobalAlloc, Layout};
 
-unsafe impl GlobalAlloc for ZeroizingAllocator {
+pub struct ZeroizingAllocator<Alloc: GlobalAlloc>(pub Alloc);
+
+unsafe fn zero(ptr: *mut u8, size: usize) {
+    for i in 0..size {
+        core::ptr::write_volatile(ptr.offset(i as isize), 0);
+    }
+    core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
+}
+
+unsafe impl<T> GlobalAlloc for ZeroizingAllocator<T>
+where
+    T: GlobalAlloc,
+{
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        System.alloc(layout)
+        self.0.alloc(layout)
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-        for i in 0..layout.size() {
-            core::ptr::write_volatile(ptr.offset(i as isize), 0);
-        }
-        core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
-        System.dealloc(ptr, layout)
+        zero(ptr, layout.size());
+        #[cfg(not(feature = "leaky"))]
+        self.0.dealloc(ptr, layout);
+    }
+
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        self.0.alloc_zeroed(layout)
     }
 }
 
-#[global_allocator]
-static GLOBAL: ZeroizingAllocator = ZeroizingAllocator;
+#[cfg(all(feature = "leaky", test))]
+mod test {
+    extern crate std;
+    use std::vec::Vec;
+
+    #[global_allocator]
+    static ALLOC: super::ZeroizingAllocator<std::alloc::System> =
+        super::ZeroizingAllocator(std::alloc::System);
+
+    #[test]
+    fn test() {
+        let mut a = Vec::with_capacity(2);
+        a.push(0xde);
+        a.push(0xad);
+        let ptr1: *const u8 = &a[0];
+        a.push(0xbe);
+        a.push(0xef);
+        let ptr2: *const u8 = &a[0];
+        assert_eq!(&[0xde, 0xad, 0xbe, 0xef], &a[..]);
+        assert_eq!(unsafe { ptr1.as_ref() }, Some(&0));
+        drop(a);
+        assert_eq!(unsafe { ptr2.as_ref() }, Some(&0));
+    }
+}
